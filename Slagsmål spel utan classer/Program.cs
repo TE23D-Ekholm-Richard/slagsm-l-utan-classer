60721e0ec7869853419c2935043c397f84ad6dc1
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2024-09-23 09:55:36 +0200

    på börjat utan classer med kinesiska variabler

---
+++ Slagsmål spel utan classer/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+int  下庞Hp = 100; // jolly
+int 末日铁拳Hp = 300; // evil
+
+
+string fighter1name = "下庞";
+string fighter2name = "末日铁拳";
+
+
+Random aas = new Random();
+
+
+
+while (下庞Hp > 0 && 末日铁拳Hp > 0 )
+{
+
+Console.WriteLine("\n ------ ===== 新一轮 ===== -------");
+Console.WriteLine($"{fighter1name}: {下庞Hp}  {fighter2name}:  {末日铁拳Hp}");
+
+
+int  下庞dmg = aas.Next(20);
+
+末日铁拳Hp -= 下庞dmg;
+末日铁拳Hp = Math.Max(0, 末日铁拳Hp);
+Console.WriteLine($"{fighter1name} åstakommer {下庞dmg} dmg på {fighter2name}");
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
