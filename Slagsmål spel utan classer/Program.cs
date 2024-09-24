5cfe5e681901e6f78e93a036f0383731a69aac85
Richard Ekholm <richard.ekholm@elev.ga.ntig.se>
2024-09-24 10:55:26 +0200

    kodat klart på mentors tiden  med kinesisk text :-)

--- Slagsmål spel utan classer/Program.cs
+++ Slagsmål spel utan classer/Program.cs
@@ -9,7 +9,7 @@ string fighter1name = "下庞";
 string fighter2name = "末日铁拳";
 
 
-Random aas = new Random();
+Random bejing = new Random();
 
 
 
@@ -20,26 +20,43 @@ Console.WriteLine("\n ------ ===== 新一轮 ===== -------");
 Console.WriteLine($"{fighter1name}: {下庞Hp}  {fighter2name}:  {末日铁拳Hp}");
 
 
-int  下庞dmg = aas.Next(20);
+int  下庞dmg = bejing.Next(50);
 
 末日铁拳Hp -= 下庞dmg;
 末日铁拳Hp = Math.Max(0, 末日铁拳Hp);
 Console.WriteLine($"{fighter1name} åstakommer {下庞dmg} dmg på {fighter2name}");
 
 
+int  末日铁拳dmg = bejing.Next(20);
+
+下庞Hp -= 末日铁拳dmg;
+下庞Hp = Math.Max(0, 下庞Hp );
+Console.WriteLine($"{fighter2name} rocket punch gör {末日铁拳dmg}dmg på {fighter1name}");
+
+Console.WriteLine("按任一按鈕");
+Console.ReadKey();
+}
+
+Console.WriteLine("\n ----- ===== 戰鬥結束了 ===== -----");
+
+if(下庞Hp == 0 &&  末日铁拳Hp == 0  )
+{
+Console.WriteLine("平局");
 
 }
 
+else if (下庞Hp == 0)
+{
+    Console.WriteLine("末日铁拳 获胜!");
+}
 
+else
+{
+    Console.WriteLine("下庞 获胜! ");
+}
 
-
-
-
-
-
-
-
-
+Console.WriteLine("按任意按钮关闭!");
+Console.ReadKey();
 
 
 
